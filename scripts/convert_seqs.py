0703126caaa04bc9f21c9d7b2f33d2be26ca4db1
laket <laket72@gmail.com>
2016-01-03 11:33:39 +0900

    change output folder path

--- scripts/convert_seqs.py
+++ scripts/convert_seqs.py
@@ -3,25 +3,140 @@
 
 import os
 import glob
+import struct
 import cv2 as cv
 
+def read_seq(path):
+    """
+    read *.seq file and return list of cv2 image files(channel BGR).
+    This skip to read the last frame of seq file.
+    """
+    
+    def read_header(ifile):
+        feed = ifile.read(4)
+        norpix = ifile.read(24)
+        version = struct.unpack('@i', ifile.read(4))
+        length = struct.unpack('@i', ifile.read(4))
+        assert(length != 1024)
+        descr = ifile.read(512)
+        params = [struct.unpack('@i', ifile.read(4))[0] for i in range(0,9)]
+        fps = struct.unpack('@d', ifile.read(8))
+        # skipping the rest
+        ifile.read(432)
+        image_ext = {100:'raw', 102:'jpg',201:'jpg',1:'png',2:'png'}
+        return {'w':params[0],'h':params[1],
+                'bdepth':params[2],
+                'ext':image_ext[params[5]],
+                'format':params[5],
+                'size':params[4],
+                'true_size':params[8],
+                'num_frames':params[6]}
+    
+    ifile = open(path, 'rb')
+    params = read_header(ifile)
+    bytes = open(path, 'rb').read()
 
-def save_img(dname, fn, i, frame):
-    cv.imwrite('{}/{}_{}_{}.png'.format(
-        out_dir, os.path.basename(dname),
-        os.path.basename(fn).split('.')[0], i), frame)
+    # this is freaking magic, but it works
+    extra = 8
+    s = 1024
+    seek = [0]*(params['num_frames']+1)
+    seek[0] = 1024
+    
+    images = []
+    
+    for i in range(0, params['num_frames'] - 1):
+        tmp = struct.unpack_from('@I', bytes[s:s+4])[0]
+        s = seek[i] + tmp + extra
+        if i == 0:
+            val = struct.unpack_from('@B', bytes[s:s+1])[0]
+            if val != 0:
+                s -= 4
+            else:
+                extra += 8
+                s += 8
+        seek[i+1] = s
+        nbytes = struct.unpack_from('@i', bytes[s:s+4])[0]
+        I = bytes[s+4:s+nbytes]
+        
+        tmp_file = '/tmp/img%d.jpg' % i
+        open(tmp_file, 'wb+').write(I)
+        
+        img = cv2.imread(tmp_file)
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        images.append(img)
+    return images
+
+def write_image_from_seq(path, path_out, format_out):
+    """
+    write jpeg files to path_out from *.seq file.
+    """
+    
+    def read_header(ifile):
+        feed = ifile.read(4)
+        norpix = ifile.read(24)
+        version = struct.unpack('@i', ifile.read(4))
+        length = struct.unpack('@i', ifile.read(4))
+        assert(length != 1024)
+        descr = ifile.read(512)
+        params = [struct.unpack('@i', ifile.read(4))[0] for i in range(0,9)]
+        fps = struct.unpack('@d', ifile.read(8))
+        # skipping the rest
+        ifile.read(432)
+        image_ext = {100:'raw', 102:'jpg',201:'jpg',1:'png',2:'png'}
+        return {'w':params[0],'h':params[1],
+                'bdepth':params[2],
+                'ext':image_ext[params[5]],
+                'format':params[5],
+                'size':params[4],
+                'true_size':params[8],
+                'num_frames':params[6]}
+    
+    ifile = open(path, 'rb')
+    params = read_header(ifile)
+    print params
+    bytes = open(path, 'rb').read()
+
+    # this is freaking magic, but it works
+    extra = 8
+    s = 1024
+    seek = [0]*(params['num_frames']+1)
+    seek[0] = 1024
+    
+    images = []
+    
+    for i in range(0, params['num_frames']-1):
+
+        tmp = struct.unpack_from('@I', bytes[s:s+4])[0]
+        s = seek[i] + tmp + extra
+        if i == 0:
+            val = struct.unpack_from('@B', bytes[s:s+1])[0]
+            if val != 0:
+                s -= 4
+            else:
+                extra += 8
+                s += 8
+        seek[i+1] = s
+        nbytes = struct.unpack_from('@i', bytes[s:s+4])[0]
+        I = bytes[s+4:s+nbytes]
+
+        path_file = os.path.join(path_out, format_out.format(i))
+        open(path_file, 'wb+').write(I)
+        
 
 out_dir = 'data/images'
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 for dname in sorted(glob.glob('data/set*')):
     for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
-        cap = cv.VideoCapture(fn)
+        setname = os.path.basename(dname)
+        vname = os.path.basename(fn).split('.')[0]
+        dirname = os.path.join(out_dir, "{}/{}".format(setname, vname))
+        format_out = "{}_{}_{}.jpg".format(setname, vname, "{}")
+        
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)        
+        
+        images = write_image_from_seq(fn, dirname, format_out)
         i = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            save_img(dname, fn, i, frame)
-            i += 1
+            
         print(fn)
--- tests/test_plot_annotations.py
+++ tests/test_plot_annotations.py
@@ -5,6 +5,8 @@ import os
 import re
 import json
 import glob
+import sys
+import cv as cv1
 import cv2 as cv
 from collections import defaultdict
 
@@ -14,39 +16,50 @@ out_dir = 'data/plots'
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
+root = "data/images"
 img_fns = defaultdict(dict)
+re_index = re.compile('_([0-9]+)\.jpg')
 
-for fn in sorted(glob.glob('data/images/*.png')):
-    set_name = re.search('(set[0-9]+)', fn).groups()[0]
+for set_name in sorted(os.listdir(root)):
     img_fns[set_name] = defaultdict(dict)
+    path_set = os.path.join(root, set_name)
 
-for fn in sorted(glob.glob('data/images/*.png')):
-    set_name = re.search('(set[0-9]+)', fn).groups()[0]
-    video_name = re.search('(V[0-9]+)', fn).groups()[0]
-    img_fns[set_name][video_name] = []
-
-for fn in sorted(glob.glob('data/images/*.png')):
-    set_name = re.search('(set[0-9]+)', fn).groups()[0]
-    video_name = re.search('(V[0-9]+)', fn).groups()[0]
-    n_frame = re.search('_([0-9]+)\.png', fn).groups()[0]
-    img_fns[set_name][video_name].append((int(n_frame), fn))
+    for video_name in sorted(os.listdir(path_set)):
+        img_fns[set_name][video_name] = []
+        path_video = os.path.join(path_set, video_name)
+        
+        for fn in sorted(os.listdir(path_video)):
+            n_frame = re_index.search(fn).group(1)
+            fullpath = os.path.join(path_video, fn)
+            img_fns[set_name][video_name].append((int(n_frame), fullpath))
 
 n_objects = 0
 for set_name in sorted(img_fns.keys()):
     for video_name in sorted(img_fns[set_name].keys()):
         wri = cv.VideoWriter(
             'data/plots/{}_{}.avi'.format(set_name, video_name),
-            cv.VideoWriter_fourcc(*'XVID'), 30, (640, 480))
+            cv1.CV_FOURCC(*'XVID'), 30, (640, 480))
+
+        if not wri.isOpened():
+            print "can't create VideoWriter. configure opencv correctly."
+            sys.exit(-1)
+        
         for frame_i, fn in sorted(img_fns[set_name][video_name]):
             img = cv.imread(fn)
+
             if str(frame_i) in annotations[set_name][video_name]['frames']:
                 data = annotations[set_name][
                     video_name]['frames'][str(frame_i)]
+
                 for datum in data:
                     x, y, w, h = [int(v) for v in datum['pos']]
                     cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
                     n_objects += 1
+
                 wri.write(img)
+
+            #cv.imshow("test", img)
+            #cv.waitKey(0)
         wri.release()
         print(set_name, video_name)
 print(n_objects)
